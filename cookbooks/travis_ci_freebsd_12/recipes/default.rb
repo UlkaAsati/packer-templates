8d968150bf1a71fd161f2762c59065500460f60b
pawelgrzeda <60094765+pawelgrzeda@users.noreply.github.com>
2020-02-17 08:04:19 +0100

    Update default.rb

--- cookbooks/travis_ci_freebsd_12/recipes/default.rb
+++ cookbooks/travis_ci_freebsd_12/recipes/default.rb
@@ -37,6 +37,8 @@ freebsd_package 'ccache'
 #ln -s /usr/local/bin/gcc10 /usr/local/bin/gcc
 #ln -s /usr/local/bin/g++10 /usr/local/bin/g++
 
+include_recipe '::pyenv'
+
 include_recipe 'travis_build_environment::rvm'
 include_recipe 'travis_build_environment::gimme'
 
@@ -49,4 +51,3 @@ freebsd_package 'gradle'
 freebsd_package 'apache-ant'
 
 include_recipe '::jdk_switcher'
-include_recipe '::pyenv'
